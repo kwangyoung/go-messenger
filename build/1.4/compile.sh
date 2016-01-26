c84e112f313f11a50f84b54d32e1f4e0f6c11206
kykim <kwangykim@sk.com>
2016-01-26 13:46:48 +0900

    godep restore failed => firewall block

--- build/1.4/compile.sh
+++ build/1.4/compile.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 set -e
+# firewall block
+mkdir -p $GOPATH/src/golang.org/x
+cd $GOPATH/src/golang.org/x
+git clone https://go.googlesource.com/crypto
 # Set directory to where we expect code to be
 cd /go/src/${SOURCE_PATH}
 echo "Downloading dependencies"
